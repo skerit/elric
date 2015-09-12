4a39db9403758fcab9313f9a7e16f49a91caa544
Jelle De Loecker <jelle@kipdola.be>
2015-09-12 21:19:58 +0200

    Begin adding rfxcom client file

--- app/assets/client_files/rfxcom_client_file.js
+++ app/assets/client_files/rfxcom_client_file.js
@@ -1,5 +1,17 @@
-module.exports = function RfxcomClientFile(client, settings) {
+/**
+ * The Rfxcom ClientFile
+ *
+ * @constructor
+ *
+ * @author   Jelle De Loecker   <jelle@develry.be>
+ * @since    1.0.0
+ * @version  1.0.0
+ */
+var Rfxcom = Function.inherits('ClientFile', function RfxcomClientFile(client, settings) {
 
-	console.log('RfxCom Client File has loaded');
+	// Call the parent constructor
+	RfxcomClientFile.super.call(this, client, settings);
 
-};
+});
+
+module.exports = Rfxcom.create;
